ca62e92e73cf6f3f3d1cebde6aaae3c15af02ead
Everton Solon <EvertonSolon@users.noreply.github.com>
2020-01-09 23:15:54 -0300

    Configuração para API usar MVC e Sql Lite, criação do banco de dados e da tabela Palavra.

--- MimicApi/MimicAPI/Startup.cs
+++ MimicApi/MimicAPI/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using MimicAPI.Database;
 
 namespace MimicAPI
 {
@@ -15,6 +17,11 @@ namespace MimicAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MimicContext>(opt =>
+            {
+                opt.UseSqlite("Data Source=Database\\Mimic.db");
+            });
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +32,7 @@ namespace MimicAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
