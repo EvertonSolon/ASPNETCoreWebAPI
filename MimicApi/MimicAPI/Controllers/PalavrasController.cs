032552672fd9596626591ce4cbced053c17a7900
Everton Solon <EvertonSolon@users.noreply.github.com>
2020-01-09 23:43:44 -0300

    Adicionado as rotas nos métodos do controller Palavra.

--- MimicApi/MimicAPI/Controllers/PalavrasController.cs
+++ MimicApi/MimicAPI/Controllers/PalavrasController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace MimicAPI.Controllers
 {
+    [Route("api/palavras")]
     public class PalavrasController : ControllerBase
     {
         private readonly MimicContext _banco;
@@ -18,17 +19,26 @@ namespace MimicAPI.Controllers
         }
 
         //App
+        //Rota -> site/api/palavras para cair no método abaixo.
+        [Route("")]
+        [HttpGet]
         public ActionResult ObterTodas()
         {
             return Ok(_banco.Palavras);
         }
 
-
+        //Web
+        //Rota -> site/api/palavras/1 para cair no método abaixo com o parâmetro 1 (id da palavra a ser buscada).
+        [Route("{id}")]
+        [HttpGet]
         public ActionResult Obter(int id)
         {
             return Ok(_banco.Palavras.Find(id));
         }
 
+        //Rota -> site/api/palavras para cair no método abaixo (POST: id, nome, ativo, pontuação, data da criação).
+        [Route("")]
+        [HttpPost]
         public ActionResult Cadastrar(Palavra palavra)
         {
             _banco.Palavras.Add(palavra);
@@ -36,6 +46,9 @@ namespace MimicAPI.Controllers
             return Ok();
         }
 
+        //Rota -> site/api/palavras/1 para cair no método abaixo com o parâmetro 1 (PUT: id, nome, ativo, pontuação, data da criação).
+        [Route("{id}")]
+        [HttpPut]
         public ActionResult Atualizar(int id, Palavra palavra)
         {
             _banco.Palavras.Update(palavra);
@@ -43,6 +56,8 @@ namespace MimicAPI.Controllers
             return Ok();
         }
 
+        [Route("{id}")]
+        [HttpDelete]
         public ActionResult Deletar(int id)
         {
             _banco.Palavras.Remove(_banco.Palavras.Find(id));
