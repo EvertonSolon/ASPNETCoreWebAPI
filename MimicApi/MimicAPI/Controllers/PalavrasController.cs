ea7fe4aef23a0bd1c49269c0439da4bca12440ca
Everton Solon <EvertonSolon@users.noreply.github.com>
2020-01-11 01:28:08 -0300

    Atualização dos retornos dos métodos do controller Palavras.

--- MimicApi/MimicAPI/Controllers/PalavrasController.cs
+++ MimicApi/MimicAPI/Controllers/PalavrasController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MimicAPI.Database;
 using MimicAPI.Models;
 using System;
@@ -33,7 +34,12 @@ namespace MimicAPI.Controllers
         [HttpGet]
         public ActionResult Obter(int id)
         {
-            return Ok(_banco.Palavras.Find(id));
+            var objeto = _banco.Palavras.Find(id);
+
+            if (objeto != null)
+                return Ok(objeto);
+
+            return NotFound(); //Ou StatusCode(404);
         }
 
         //Rota -> site/api/palavras para cair no método abaixo (POST: id, nome, ativo, pontuação, data da criação).
@@ -44,7 +50,8 @@ namespace MimicAPI.Controllers
             _banco.Palavras.Add(palavra);
             _banco.SaveChanges();
 
-            return Ok();
+            //return Ok(palavra);
+            return Created($"/api/palavras/{palavra.Id}", palavra);
         }
 
         //Rota -> site/api/palavras/1 para cair no método abaixo com o parâmetro 1 (PUT: id, nome, ativo, pontuação, data da criação).
@@ -52,19 +59,33 @@ namespace MimicAPI.Controllers
         [HttpPut]
         public ActionResult Atualizar(int id, [FromBody]Palavra palavra)
         {
+            var objeto = _banco.Palavras.AsNoTracking().FirstOrDefault(x => x.Id == id);
+
+            if (objeto == null)
+                return NotFound();
+
+            palavra.Id = id;
             _banco.Palavras.Update(palavra);
             _banco.SaveChanges();
 
-            return Ok();
+            return Ok(palavra);
         }
 
         [Route("{id}")]
         [HttpDelete]
         public ActionResult Deletar(int id)
         {
-            _banco.Palavras.Remove(_banco.Palavras.Find(id));
+            var palavra = _banco.Palavras.Find(id);
 
-            return Ok();
+            if (palavra == null)
+                return NotFound();
+
+            //_banco.Palavras.Remove(obj);
+            palavra.Ativo = false;
+            _banco.Update(palavra);
+            _banco.SaveChanges();
+
+            return NoContent();
         }
     }
 }
